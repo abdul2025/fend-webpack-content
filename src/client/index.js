8257be77d64159cf82f25a32e3370ddf6b14accc
abdul2020 <git.generalassemb.ly>
2020-05-20 04:02:59 +0300

    new loaders file-loader

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,6 @@
-alert('i exsit');
-alert('work in a new branch');
+import { checkForName } from './js/nameChecker';
+import { handleSubmit } from './js/formHandler';
+import img from './images/fogy.jpg';
+console.log(img);
+console.log(checkForName);
+alert('I EXIST');
--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,21 @@ const webpack = require('webpack');
 
 module.exports = {
 	entry: './src/client/index.js',
+	module: {
+		rules: [
+			{
+				test: '/.js$/', // any file ends with .js
+				exclude: /node_modules/, // except node files
+				loader: 'babel-loader', // loed the babel
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+					},
+				],
+			},
+		],
+	},
 };
